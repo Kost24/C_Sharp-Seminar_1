7a3f9d0b2b7ed945ed097eec5ddeb8d4e1a26a74
KOST <KSK>
2024-02-10 15:13:02 +0200

    add Task 001: Решение задания №1

--- Task 001/Program.cs
+++ Task 001/Program.cs
@@ -31,36 +31,41 @@
 // На выходе: числа равны
 
 
-public class Answer {
+public class Answer
+{
     static void CompareNumbers(int firstNumber, int secondNumber)
     {
         // Введите свое решение ниже.  Мое решение.
 
-       if (firstNumber > secondNumber)
-       {
-         Console.WriteLine("первое число больше");
-       } 
-         else if (firstNumber < secondNumber)
-       {
-         Console.WriteLine("второе число больше");
-       } 
-         else if (firstNumber == secondNumber)
-       {
-         Console.WriteLine("числа равны");
-       }
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine("первое число больше");
+        }
+        else if (firstNumber < secondNumber)
+        {
+            Console.WriteLine("второе число больше");
+        }
+        else if (firstNumber == secondNumber)
+        {
+            Console.WriteLine("числа равны");
+        }
 
     }
 
 
-  // Не удаляйте и не меняйте метод Main! 
-    static public void Main(string[] args) {
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args)
+    {
         int firstNumber, secondNumber;
 
-        if (args.Length >= 2) {
+        if (args.Length >= 2)
+        {
             firstNumber = int.Parse(args[0]);
             secondNumber = int.Parse(args[1]);
-        } else {
-           // Здесь вы можете поменять значения для отправки кода на Выполнение
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
             firstNumber = 9;
             secondNumber = 3;
         }
